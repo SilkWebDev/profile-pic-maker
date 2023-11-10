0ea449bca9b07ad314f539e8d5d0838dcd7ad355
SilkWebDev <silk-web-dev@proton.me>
2023-11-10 14:30:41 +0700

    Creating static folder

---
+++ client/static/scripts/main.js
@@ -0,0 +1,37 @@
+// const IMG_UPLOAD = document.getElementById('image-upload');
+// const IMG_FIELD = document.getElementById('image-field');
+// const IMG_PREVIEW = document.getElementById('image-preview');
+// const IMG_PREVIEW_IMAGE = document.querySelector('#image-preview img');
+
+// // Register events
+// IMG_UPLOAD.addEventListener('mouseenter', toggleDragging);
+// IMG_UPLOAD.addEventListener('mouseleave', toggleDragging);
+// IMG_FIELD.addEventListener('change', handleFileChange);
+
+// //
+// function toggleDragging() {
+//   this.classList.toggle('dragging');
+// }
+
+// function handleFileChange(e) {
+//   e.preventDefault();
+
+//   if (this.files.length === 0) {
+//     //
+//     IMG_PREVIEW.classList.add('hidden');
+//     return;
+//   }
+
+//   const file = this.files[0];
+//   // IMG_PREVIEW_IMAGE.src = URL.createObjectURL(file);
+//   IMG_PREVIEW.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
+//   IMG_PREVIEW.classList.remove('hidden');
+// }
+
+// function setImagePreview() {}
+
+// function clearImageUpload() {}
+
+// function showMessage() {}
+
+
